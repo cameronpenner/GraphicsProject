a011993f6f7c23d0471061d3471d617d7ba260b4
cameron penner <camdpenner@gmail.com>
2016-04-02 16:26:38 -0500

    chunks load

--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 public class WorldGenerator : MonoBehaviour
 {
 	private WorldSampler _sampler;
+	private Transform _player;
 
 	[SerializeField]
-	private GameObject _player;
+	private GameObject _playerPrefab;
 
 	[SerializeField]
 	private GameObject _chunkPrefab;
+
+	[SerializeField]
+	private int _viewDistance = 6;
 
 	private List<Chunk> _loadedChunks;
 
@@ -18,8 +22,25 @@ public class WorldGenerator : MonoBehaviour
 		_sampler = new WorldSampler();
 		_loadedChunks = new List<Chunk>();
 
-		GenerateChunk(0, 0, 0);
-		GenerateChunk(1, 0, 0);
+		_player = ((GameObject)Instantiate(_playerPrefab, new Vector3(0, 20, 0), Quaternion.Euler(0, 0, 0))).transform;
+
+		//GenerateChunk(0, 0, 0);
+		int playerx = (int)Mathf.Floor(_player.position.x / Chunk.ChunkSize.x);
+		int playerz = (int)Mathf.Floor(_player.position.z / Chunk.ChunkSize.z);
+
+		int startx = playerx - _viewDistance / 2;
+		int startz = playerz - _viewDistance / 2;
+		int endx = playerx + _viewDistance / 2;
+		int endz = playerz + _viewDistance / 2;
+
+		for(int x = startx; x <= endx; x++)
+		{
+			for(int z = startz; z <= endz; z++)
+			{
+				GenerateChunk(x, 0, z);
+			}
+		}
+
 	}
 
 	private void Update()
@@ -28,7 +49,7 @@ public class WorldGenerator : MonoBehaviour
 
 	private Chunk GenerateChunk(int chunkX, int chunkY, int chunkZ)
 	{
-		GameObject chunkGO = (GameObject)Instantiate(_chunkPrefab, new Vector3(0,0,0), Quaternion.Euler(0, 0, 0));
+		GameObject chunkGO = (GameObject)Instantiate(_chunkPrefab, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
 		Chunk chunk = chunkGO.GetComponent<Chunk>();
 		chunk.Initialize(new Vector3(chunkX, chunkY, chunkZ));
 
@@ -38,9 +59,10 @@ public class WorldGenerator : MonoBehaviour
 			{
 				for(int z = 0; z < Chunk.ChunkSize.z; z++)
 				{
-					var voxel = _sampler.SamplePosition((int)(chunkX*Chunk.ChunkSize.x + x), (int)(chunkY * Chunk.ChunkSize.y + y), (int)(chunkZ * Chunk.ChunkSize.z) + z);
+					var voxel = _sampler.SamplePosition((int)(chunkX * Chunk.ChunkSize.x + x), (int)(chunkY * Chunk.ChunkSize.y + y), (int)(chunkZ * Chunk.ChunkSize.z) + z);
 
 					chunk.SetVoxel(voxel, x, y, z);
+					_loadedChunks.Add(chunk);
 				}
 			}
 		}
--- Assets/WorldSampler.cs
+++ Assets/WorldSampler.cs
@@ -4,14 +4,16 @@ internal class WorldSampler
 {
 	private float TerrainAmplitude = 10;
 	private float TerrainOffset = 4;
-	private float NoiseScale = 0.05f;
+	private float NoiseScale = 0.02f;
+
+	private float NoiseOffset = 1000;
 
 	public Voxel SamplePosition(int x, int y, int z)
 	{
 		Voxel voxel = new Voxel();
 		voxel.on = false;
 
-		var height = TerrainAmplitude * Mathf.PerlinNoise(NoiseScale * x, NoiseScale * z) + TerrainOffset;
+		var height = TerrainAmplitude * Mathf.PerlinNoise(NoiseScale * x + NoiseOffset, NoiseScale * z + NoiseOffset) + TerrainOffset;
 		//Debug.Log(height);
 		//Debug.Log("Height at [" + x + ", " + y+"]: " + Mathf.PerlinNoise(NoiseScale * x, NoiseScale * z));
 		if(y < height)
